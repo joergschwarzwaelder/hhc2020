15fee6e02d304bc6f648caf1ac30b1e69795cb5b
joergschwarzwaelder <76226054+joergschwarzwaelder@users.noreply.github.com>
2020-12-20 19:12:23 +0100

    Create vending-password.sh

---
+++ Additional/vending-password.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+use strict;
+
+my $file="vending-machines.json";
+
+my $password=`grep password ../$file`;
+chop $password;
+$password=~s/^.*: "//;
+$password=~s/"//;
+
+my $solved=" " x length($password);
+
+foreach my $i ('0'..'9','a'..'z','A'..'Z'){
+  unlink $file;
+  open(F,"|./vending-machines > /dev/null 2>&1");
+  printf F "%s\n",$i x 8;
+  printf F "%s\n",$i x 8;
+  printf F "\n";
+  close F;
+  my $t=`grep password $file`;
+  chop $t;
+  $t=~s/^.*: "//;
+  $t=~s/"//;
+  printf "%s - %s",$i,$t;
+
+  for(my $c=0;$c<length($password);$c++){
+    if(substr($password,$c,1) eq substr($t,$c%8,1)){
+      substr($solved,$c,1)=$i;
+      substr($password,$c,1)=" ";
+    }
+  }
+  printf " - *$solved*\r";
+  last unless($solved=~/ /);
+}
+
+ print "\n\nFinally: *".$solved."*\n";
